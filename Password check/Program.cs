98cc5708077c7dd706bd6120243a5b852919070a
davit <davit@GOSH>
2024-09-24 14:09:05 +0400

    Add project files.

---
+++ Password check/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("GamarjobaSheiyvanet paroli:");
+
+string Password = Console.ReadLine();
+
+if (Password == "secret123")
+{
+    Console.WriteLine("Access Granted");
+}
+else if (Password != "secret123")
+{
+    Console.WriteLine("Access Forbidden");
+
+}
+
+
+
+Console.ReadLine();
